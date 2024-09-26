5f3ff15371d08a069bf7d497f950320f328fe064
Harsha Garine <garineharshavardhan@gmail.com>
2024-09-26 02:52:58 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {  
-    basePath: '/my-website', // Replace 'repo-name' with the actual name of your GitHub repository
-    assetPrefix: '/my-website/tree/main/public', // This ensures the correct path for static assets}
-}
+const nextConfig = {}
 
 module.exports = nextConfig
